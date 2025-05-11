e3964fe5b7f8548bee281ea8ef2a0bebb48b7e43
HarunHudaiTan <harun2002tan@gmail.com>
2025-05-11 09:51:35 +0300

    Agent Related classes updated

--- Search/Agent.py
+++ Search/Agent.py
@@ -5,16 +5,17 @@ from google.genai import types
 import json
 class Agent:
 
-    def __init__(self, system_prompt,response_mime_type,api_key):
+    def __init__(self, system_prompt,response_mime_type):
 
         self.system_prompt = system_prompt
         self.response_mime_type = response_mime_type
-        self.api_key = api_key
-
-
 
     def response(self,prompt):
-        client=genai.Client(api_key=self.api_key)
+        import os
+        from dotenv import load_dotenv
+        load_dotenv()
+        api_key = os.getenv("GOOGLE_API_KEY")
+        client=genai.Client(api_key=api_key)
         response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=prompt,
