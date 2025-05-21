a54a212014cdfee97af32a985f0fd3556ede5d26
Ahmet Arda Bağdatlı <ahmetardabag@outlook.com>
2025-05-21 20:35:01 +0300

    Add Alternate Version Of Market Pipeline

--- test_script_3.py
+++ test_script_3.py
@@ -1,7 +1,7 @@
-from proj_market_pipeline import LLM_Market_Pipeline
+from proj_market_pipeline_agent import LLM_Market_Pipeline
 
 if __name__ == "__main__":
     pipeline = LLM_Market_Pipeline()
     #writing the output as a json file
-    response = pipeline.run_market_pipeline("Chicken Wings")
-    print(response)
+    response = pipeline.run_market_pipeline("I can leave about 10000 liras a month for groceries. I have a family of 5")
+    print(response.text)
---
+++ test_script_4.py
@@ -0,0 +1,7 @@
+from proj_market_pipeline import LLM_Market_Pipeline
+
+if __name__ == "__main__":
+    pipeline = LLM_Market_Pipeline()
+    #writing the output as a json file
+    response = pipeline.run_market_pipeline("Buğday Unu")
+    print(response.text)
